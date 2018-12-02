6dd455eb59f038103f9f1b1ccdf0c7a5871525c9
Vinicius Barbosa <viniciusnep1@hotmail.com>
2018-12-01 22:43:44 -0200

    adicionando rotas e algumas views usando o ejs

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var app = express();
+
+app.set('view engine', 'ejs');
+
 app.get('/', function(request, response){
-    response.send("<html><body>Funcionando</body></html>")
+    response.render("home/index")
 })
+
+app.get('/formulario_noticia', function(request, response){
+    response.render("admin/form_add_noticia")
+})
+
+
+app.get('/noticias', function(request, response){
+    response.render("noticias/noticia")
+})
+
 app.listen(3000, function(){
     console.log('Servidor rodando com Express na porta 3000');
 });
